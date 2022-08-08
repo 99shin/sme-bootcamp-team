e7449b8e9b9f14bfb371665974c4caee001177f6
jun <990114js@naver.com>
2022-08-08 21:01:09 +0900

    node upload

--- challenge/src/views/3-2.vue
+++ challenge/src/views/3-2.vue
@@ -3,7 +3,7 @@
     <div class="search_challenge">
         챌린지명 검색
         <!-- <SearchBar hint="Search hint" :text="searchPhrase" @textChange="onTextChanged" @submit="onSubmit" /> -->
-        <input type="text">
+        <input type="text"> 검색 아이콘
     </div>
     <div class="search_challenge_list">
         검색된 챌린지 목록
